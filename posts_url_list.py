ce2519e5c09827425b9070a15d9cab352f580216
Nafiz Ali <77394404+devnafizali@users.noreply.github.com>
2024-04-06 18:57:05 +0600

    Update posts_url_list.py

--- posts_url_list.py
+++ posts_url_list.py
@@ -19,7 +19,7 @@ async def fetch_post_urls(pageId):
         await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1')
         await page.setViewport({'width': 390, 'height': 844, 'deviceScaleFactor': 3, 'isMobile': True, 'hasTouch': True})
         
-        await page.goto("https://m.facebook.com/"+pageId)
+        await page.goto("https://m.facebook.com/"+pageId+"/posts")
         await page.waitForSelector('[data-comp-id="22222"][data-type="container"]')
         container_to_remove = await page.querySelector('[data-comp-id="22222"][data-type="container"]')
 
