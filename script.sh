49e7b354f528208b9bbc4453dd22867fda085b86
Nafiz Ali <77394404+devnafizali@users.noreply.github.com>
2024-04-09 20:33:06 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,12 +5,15 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
-
 # URL argument passed to the script
 url="$1"
 
-# Run the Node.js script to fetch postUrls
-node mainjs.js
+# Extract the last part or name from the URL using awk or sed
+# Here, we use awk to extract the last field separated by '/'
+name=$(echo "$url" | awk -F'/' '{print $NF}')
+
+# Run the Node.js script with the extracted name as an argument
+node mainjs.js "https://mobile.facebook.com/$name"
 
 # Read the postUrls from the file and pass them to the Python script along with the URL
 python3 scrapper.py "$url"
