7101beeb39872251e85f526d254ced22545dc543
Nafiz Ali <77394404+devnafizali@users.noreply.github.com>
2024-04-30 16:06:57 +0600

    update

--- script2.sh
+++ script2.sh
@@ -1,35 +1,51 @@
+# # #!/bin/bash
+
+# # # Open Google Chrome
+# # "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
+# # sleep 10
+
+# # # Use PowerShell to invoke the REST API endpoint and retrieve the JSON data
+# # data=$(powershell.exe -Command "(Invoke-WebRequest -Uri 'http://127.0.0.1:9222/json/version' | ConvertFrom-Json)")
+
+# # # Print the JSON data
+# # echo "$data"
+
+# # # Close Chrome using PowerShell
+# # powershell.exe -Command "Stop-Process -Name chrome -Force"
+
+
+
 # #!/bin/bash
 
 # # Open Google Chrome
 # "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
-# sleep 10
 
-# # Use PowerShell to invoke the REST API endpoint and retrieve the JSON data
-# data=$(powershell.exe -Command "(Invoke-WebRequest -Uri 'http://127.0.0.1:9222/json/version' | ConvertFrom-Json)")
+# # google-chrome --user-data-dir="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
 
-# # Print the JSON data
+# data=$(curl -s http://127.0.0.1:9222/json/version)
+
+# # Print the fetched data
 # echo "$data"
 
-# # Close Chrome using PowerShell
+
+# # Close Chrome using pkill
+# # pkill chrome
 # powershell.exe -Command "Stop-Process -Name chrome -Force"
-
-
-
 #!/bin/bash
 
-# Open Google Chrome
-# "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
-
+# Start Chrome in headless mode
 google-chrome --user-data-dir="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
 
+# Wait for Chrome to start
+sleep 2
+
+# Use curl to fetch data from the specified URL
 data=$(curl -s http://127.0.0.1:9222/json/version)
 
 # Print the fetched data
 echo "$data"
 
-
-# Close Chrome using pkill
+# Close Chrome
 pkill chrome
-# powershell.exe -Command "Stop-Process -Name chrome -Force"
 
 
