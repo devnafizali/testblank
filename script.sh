a00b102171f753f51668563369117c7fd6ba77a1
Nafiz Ali <77394404+devnafizali@users.noreply.github.com>
2024-04-30 17:13:14 +0600

    upload

--- script.sh
+++ script.sh
@@ -7,12 +7,16 @@ if [ "$#" -ne 1 ]; then
 fi
 # URL argument passed to the script
 url="$1"
-google-chrome --user-data-dir="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
+# Start Chrome in headless mode
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
+
+# google-chrome --user-data-dir="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
 
 # Wait for Chrome to start
 sleep 2
 
 # Use curl to fetch data from the specified URL
 data=$(curl -s http://127.0.0.1:9222/json/version)
+
 name=$(echo "$url" | awk -F'/' '{print $NF}')
-python3 get_links.py "$name" "$data"
+python get_links.py "$name" "$data"
