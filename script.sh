ba93cce58e96c61131105da86cfb7a1d4522e2c9
Nafiz Ali <77394404+devnafizali@users.noreply.github.com>
2024-04-30 17:18:00 +0600

    upd

--- script.sh
+++ script.sh
@@ -1,3 +1,26 @@
+# #!/bin/bash
+
+# # Check if the URL argument is provided
+# if [ "$#" -ne 1 ]; then
+#     echo "Usage: $0 <URL>"
+#     exit 1
+# fi
+# # URL argument passed to the script
+# url="$1"
+# # Start Chrome in headless mode
+# "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
+
+# # google-chrome --user-data-dir="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
+
+# # Wait for Chrome to start
+# sleep 2
+
+# # Use curl to fetch data from the specified URL
+# data=$(curl -s http://127.0.0.1:9222/json/version)
+
+# name=$(echo "$url" | awk -F'/' '{print $NF}')
+# python3 get_links.py "$name" "$data"
+
 #!/bin/bash
 
 # Check if the URL argument is provided
@@ -5,12 +28,12 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
+
 # URL argument passed to the script
 url="$1"
-# Start Chrome in headless mode
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
 
-# google-chrome --user-data-dir="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
+# Start Chrome in headless mode (assuming Chrome is installed)
+google-chrome --user-data-dir="Default" --headless --disable-gpu --remote-debugging-port=9222 --disable-gpu http://127.0.0.1:9222/json/version &
 
 # Wait for Chrome to start
 sleep 2
@@ -19,4 +42,4 @@ sleep 2
 data=$(curl -s http://127.0.0.1:9222/json/version)
 
 name=$(echo "$url" | awk -F'/' '{print $NF}')
-python get_links.py "$name" "$data"
+python3 get_links.py "$name" "$data"
